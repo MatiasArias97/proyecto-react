fb4d47729aa43e0e0dc51873462cbecb80f7e62f
MatiasArias97 <rodriguezceciliadelpilar@gmail.com>
2024-12-19 18:18:15 -0300

    errores

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://MatiasArias97.github.io/proyecto-react"
+  base: ""
 
 })
